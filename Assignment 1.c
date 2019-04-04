739298e587d2bdf50ac5e57c55b40b579a3aa47b
c3095065 <48082038+c3095065@users.noreply.github.com>
2019-04-04 17:57:45 +1100

    Add files via upload
    comment

---
+++ Assignment 1.c
@@ -0,0 +1,24 @@
+/*Simon West Student No. c3095065
+ENGG1003 Introduction to programming---Programming-Assignment-1
+English Text Ciphers*/
+
+
+#include <stdio.h>
+
+float add(float sum);
+float sum,a,b;
+main(){
+	printf("enter value for 'a'");
+	scanf("%f",&a);
+	printf("enter value for 'b'");
+	scanf("%f",&b);
+printf("a + b =%f", sum(a,b));
+
+return(0)};
+
+float add(sum=a+b){
+float sum,a,b;
+a=&n;
+b=&n;
+return(sum);
+}
